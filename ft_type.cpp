3b31a2233e863d4386bdd9145045e9917794b57a
Your Name <you@example.com>
2023-10-31 12:32:08 +0100

    @abouramd

---
+++ ft_type.cpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <vector>
+#include <iostream>
+
+std::vector<std::string> my_split(const std::string str);
+
+std::map<std::string, std::string> fileTypes;
+std::map<std::string, std::string> fileTypesRev;
+
+std::string getRevContentType(const std::string & ext)
+{
+
+}
+
+std::string getContentType(const std::string & ext)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
