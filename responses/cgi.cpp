e87d2719574ad2224168d558e56336cb185bb008
Rachid Amhouch <ramhouch@e2r12p8.1337.ma>
2023-11-04 14:56:58 +0100

    time out step

--- responses/cgi.cpp
+++ responses/cgi.cpp
@@ -4,30 +4,31 @@
 
 char **get_env(Client &client, std::string &get_query)
 {
+    (void)client;
     std::map<std::string, std::string>  env;
-    env["AUTH_TYPE"] = "";
-    env["CONTENT_LENGTH"] = client.headers["Content-Length"];
-    env["CONTENT_TYPE"] = client.headers["Content-Type"];
+    // env["AUTH_TYPE"] = "";
+    // env["CONTENT_LENGTH"] = client.headers["Content-Length"];
+    // env["CONTENT_TYPE"] = client.headers["Content-Type"];
     //env["PATH_INFO"] = _client.req.get_path();
     //env["PATH_TRANSLATED"] = _client.req.real_uri;
     env["QUERY_STRING"] = get_query;
-    env["REMOTE_HOST"] = client.headers["Host"];
-    env["REMOTE_USER"] = "";
-    env["HTTP_COOKIE"] = client.headers["Cookie"];;
-    env["REMOTE_IDENT"] = "";
-    env["REQUEST_METHOD"] = client.method;
-    env["GATEWAY_INTERFACE"] = "CGI/1.1";
+    // env["REMOTE_HOST"] = client.headers["Host"];
+    // env["REMOTE_USER"] = "";
+    // env["HTTP_COOKIE"] = client.headers["Cookie"];;
+    // env["REMOTE_IDENT"] = "";
+    // env["REQUEST_METHOD"] = client.method;
+    // env["GATEWAY_INTERFACE"] = "CGI/1.1";
     // env["REQUEST_URI"] = _client.req.uri;
     // env["SCRIPT_NAME"] = _client.req.current_location.cgi_path;
-    // env["SCRIPT_FILENAME"] = _client.req.current_location.root + "/" + _client.req.current_location.cgi_path;
+    env["SCRIPT_FILENAME"] = "www/realtest/process_form.php";
     // env["SERVER_NAME"] = _client.server_ptr->name;
-    env["SERVER_PROTOCOL"] = "HTTP/1.1";
+    // env["SERVER_PROTOCOL"] = "HTTP/1.1";
     // env["SERVER_PORT"] = std::to_string(_client.server_ptr->port);
-    env["SERVER_SOFTWARE"] = "webserv/1.0";
-    env["REDIRECT_STATUS"] = "200";
-    env["REQUEST_SCHEME"] = "http";
-    env["HTTPS"] = "off";
-    env["SERVER_SIGNATURE"] = "webserv/1.0";
+    // env["SERVER_SOFTWARE"] = "webserv/1.0";
+    // env["REDIRECT_STATUS"] = "200";
+    // env["REQUEST_SCHEME"] = "http";
+    // env["HTTPS"] = "off";
+    // env["SERVER_SIGNATURE"] = "webserv/1.0";
 
     char** keyValueArray = new char*[env.size() + 1];
 
@@ -56,14 +57,13 @@ void cgi(Client &client, std::string &get_query)
     if (!(client.pid = fork()))
     {
         char** env = get_env(client, get_query);
-        int i = 0;
-        while (env[i])
-        {
-            std::cout << env[i] << std::endl;
-            i++;
-        }
+        // int i = 0;
+        // while (env[i])
+        // {
+        //     std::cout << env[i] << std::endl;
+        //     i++;
+        // }
         
-        std::cout << get_query << std::endl;
         std::string Query = "QUERY_STRING=" + get_query;
         dup2(fd, 1);
         char* argv[] = { const_cast<char*>(client.server->second.cgi.second["php"].c_str()), const_cast<char*>(client.target.c_str()), NULL };
