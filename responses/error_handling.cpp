5626f448c47db712d0f78de743cd7d5ab7380e56
abdelhay bouramdane <abdelhaybouramdane@gmail.com>
2023-11-10 17:16:16 +0100

    @abouramd

--- responses/error_handling.cpp
+++ responses/error_handling.cpp
@@ -7,28 +7,28 @@ void error_handling(Client &client)
     if (client.statusCode == 403)
     {
         client.fullPath = "error_pages/403.html";
-        client.state_string = "403 - created";
+        client.state_string = "403 Forbidden";
         client.method = "GET";
         return;
     }
     if (client.statusCode == 404)
     {
         client.fullPath = "error_pages/404.html";
-        client.state_string = "404 - not found";
+        client.state_string = "404 Not Found";
         client.method = "GET";
         return;
     }
     if (client.statusCode == 501)
     {
         client.fullPath = "error_pages/501.html";
-        client.state_string = "501 - created";
+        client.state_string = "501 Not Implemented";
         client.method = "GET";
         return;
     }
     if (client.statusCode == 413)
     {
         client.fullPath = "error_pages/413.html";
-        client.state_string = "413 - created";
+        client.state_string = "413 Request Entity Too Large";
         client.method = "GET";
         return;
     }
@@ -49,21 +49,21 @@ void error_handling(Client &client)
     if (client.statusCode == 505)
     {
         client.fullPath = "error_pages/505.html";
-        client.state_string = "505  HTTP Version Not Supported";
+        client.state_string = "505 HTTP Version Not Supported";
         client.method = "GET";
         return;
     }
     if (client.statusCode == 415)
     {
         client.fullPath = "error_pages/415.html";
-        client.state_string = "415 - Unsupported Media Type";
+        client.state_string = "415 Unsupported Media Type";
         client.method = "GET";
         return;
     }
     if (client.statusCode == 201)
     {
         client.fullPath = "error_pages/201.html";
-        client.state_string = "201 - created";
+        client.state_string = "201 created";
         client.method = "GET";
         return;
     }
